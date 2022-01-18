8d08122ef14c3b36fc2c908ae5029bf9a81600a7
jaime sanchez galvis <jaimesanchezgalvis@gmail.com>
2022-01-18 18:54:18 -0500

    buil1.1

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const Main = () => {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className='"card mb-3'>
+                <img className='card-img-top' src={process.env.PUBLIC_URL + "/images/boletaRifa.jpeg"} alt="" />
+                </div>
+                <h3 className='card-title"' >Rifa Benefica</h3>
+            </header>
+        </div>
+
+    );
+};
+
+export default Main;
